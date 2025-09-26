c138b34f714d2e0942a8bbca5a43b33188b24990
shimnasherin77-prog <shimnasherin77@gmail.com>
2025-09-26 06:50:58 +0100

    aggregate and filtering

--- aggfil1.py
+++ aggfil1.py
@@ -83,3 +83,7 @@ arr1=arr[(arr < 40)|(arr > 90)]
 print(arr1)
 print("sum:",np.sum(arr1))
 print("count:",np.size(arr1))
+
+# 11. Create a 5×5 array of random integers between 1 and 100
+# → Filter elements that are multiples of 5
+# → Find their sum, mean, and total count
